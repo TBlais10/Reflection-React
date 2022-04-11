4de876ac6aebda9f08a5edeefd51e3914ba1f77f
TBlais10 <blaislighting@gmail.com>
2022-04-10 23:34:09 -0400

    Found images for pages, continued working on home, added navbar

--- reflection/src/common/Routers/AppRouter.js
+++ reflection/src/common/Routers/AppRouter.js
@@ -1,10 +1,12 @@
 import React from "react";
 import {Route, Routes} from 'react-router-dom'
-import Home from "../../components/Home";
+import Home from "../../components/Home/Home";
+import Navbar from "../../components/Navbar/Navbar";
 
 const AppRouter = () => {
     return (
         <div style = {{width: "100%"}}>
+        <Navbar />
         <Routes>
             <Route path="/" element = {<Home />} />
         </Routes>
--- reflection/src/components/Home/Home.js
+++ reflection/src/components/Home/Home.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Container from "../common/Container";
-import Button from "../common/Button";
-import FaButton from '../faCommon/FaButton'
+import Container from "../../common/Container";
+import Button from "../../common/Button";
+import FaButton from '../../faCommon/FaButton'
 
 const Home = () => {
     return(
-            <Container style = {{backgroundColor: "goldenrod"}}>
+            <Container>
                 <h1>Welcome to Reflection</h1>
                 <h2>Share your thoughts, share with others</h2>
             </Container>
